559c91ae1060f5ce737ca26a5b9e1d28b30ddbff
AksanaLapkou <aksana.lapkouskaya@analyticpartners.com>
2024-04-22 10:08:03 -0500

    Add driver

--- planner_regression.py
+++ planner_regression.py
@@ -2,16 +2,36 @@ import pytest
 from playwright.sync_api import Page
 from management_page import ManagementPage
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+# This tests depend on each other
 @pytest.mark.smoke
 def test_create_scenario(page: Page) -> None:
     management_page = ManagementPage(page)
     management_page.navigate_to_scenario()
     management_page.create_scenario()
     logging.info("Scenario Created")
+
+
+def test_edit_scenario(page: Page) -> None:
+    management_page = ManagementPage(page)
+    management_page.update_scenario()
+    logging.info("Scenario was updated")
+
+
+def test_add_driver(page: Page) -> None:
+    management_page = ManagementPage(page)
+    management_page.add_driver()
+    logging.info("Driver was added")
+
+
+def test_add_al_driver(page: Page) -> None:
+    management_page = ManagementPage(page)
+    management_page.add_al_driver()
+    logging.info("AL Driver was added")
 
 
 @pytest.mark.regression
@@ -26,6 +46,7 @@ def test_save_scenario(page: Page) -> None:
 @pytest.mark.regression
 def test_copy_scenario(page: Page) -> None:
     management_page = ManagementPage(page)
+    management_page.navigate_to_scenario()
     management_page.copy_scenario_button.click()
     logging.info("Scenario was copied")
 
@@ -35,7 +56,6 @@ def test_delete_scenario(page: Page) -> None:
     management_page = ManagementPage(page)
     management_page.delete_scenario()
     logging.info("Scenarios were deleted")
-
 
 # @pytest.mark.smoke
 # def test_save_as_scenario(page: Page) -> None:
